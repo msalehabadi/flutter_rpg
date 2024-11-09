9aad25cc65f9cc9c30ec9d6014b1cbcd734e2d2c
mohammad <md.salehabadi@gmail.com>
2024-11-09 16:55:41 +0330

    first init

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Charecters'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: const Text('Home'),
+      ),
+    );
+  }
+}
