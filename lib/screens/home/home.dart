dd1aaf392e2c12232f466463d5de043576d90883
mohammad <md.salehabadi@gmail.com>
2024-11-17 13:16:46 +0330

    add google-fonts for app

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -13,7 +13,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Charecters'),
+        title: const StyledTitle('Your Charecters'),
         centerTitle: true,
       ),
       body: Container(
